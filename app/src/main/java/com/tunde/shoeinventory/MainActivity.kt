66530b9eb0319a98dfe5e9986895accd72d5931a
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-11-30 15:27:24 +0100

    Initial Commit

---
+++ app/src/main/java/com/tunde/shoeinventory/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tunde.shoeinventory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
