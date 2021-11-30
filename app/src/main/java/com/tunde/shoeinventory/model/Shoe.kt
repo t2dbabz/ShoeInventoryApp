0bcca62806edae6891752108e1c63f23d2f703ac
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-11-30 15:58:34 +0100

    add Shoe model

---
+++ app/src/main/java/com/tunde/shoeinventory/model/Shoe.kt
@@ -0,0 +1,9 @@
+package com.tunde.shoeinventory.model
+
+data class Shoe(
+    var name: String,
+    var size: Int,
+    var company: String,
+    var color: String,
+    var description: String
+    )
