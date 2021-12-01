4676abace4144ab9f8cd23d81cebaed83b5f6f0c
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-12-01 15:42:32 +0100

    add ShoeViewModel & implement LiveData for shoe list

---
+++ app/src/main/java/com/tunde/shoeinventory/viewmodel/ShoeViewModel.kt
@@ -0,0 +1,28 @@
+package com.tunde.shoeinventory.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tunde.shoeinventory.model.Shoe
+
+class ShoeViewModel(): ViewModel() {
+    private val shoeList = mutableListOf<Shoe>()
+
+    var shoeName = ""
+    var shoeSize = 0.0
+    var shoeCompany = ""
+    var shoeColor = ""
+    var shoeDescription = ""
+
+    private val _shoeListData = MutableLiveData<List<Shoe>>()
+    val shoeListData : LiveData<List<Shoe>>
+    get() = _shoeListData
+
+
+    fun addShoe() {
+        val shoe = Shoe(shoeName, shoeSize, shoeCompany, shoeColor, shoeDescription)
+        shoeList.add(shoe)
+        _shoeListData.value = shoeList
+    }
+}
