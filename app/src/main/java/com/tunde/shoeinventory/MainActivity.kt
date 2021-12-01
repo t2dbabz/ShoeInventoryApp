1ab0d4b3234da8683ac6e13b854b711463701005
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-12-01 20:26:53 +0100

    Use dataBinding to inflate the activity_main layout

--- app/src/main/java/com/tunde/shoeinventory/MainActivity.kt
+++ app/src/main/java/com/tunde/shoeinventory/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tunde.shoeinventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -18,8 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(
             R.id.nav_host_fragment_container
