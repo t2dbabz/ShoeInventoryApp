2784a938579aac62778dfc8ab3b525f6197b422d
Babatunde Owoleke <tundeowoleke1@gmail.com>
2021-12-01 08:15:32 +0100

    Set up navController with AppBarConfiguration

--- app/src/main/java/com/tunde/shoeinventory/MainActivity.kt
+++ app/src/main/java/com/tunde/shoeinventory/MainActivity.kt
@@ -2,10 +2,42 @@ package com.tunde.shoeinventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.tunde.shoeinventory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment_container)
+        return super.onSupportNavigateUp() || navController.navigateUp(appBarConfiguration)
     }
 }
